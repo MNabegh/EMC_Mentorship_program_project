42f140f021738052c7a2f3ed63b452474aaed910
Mahmoud Nabegh <mahmoudnabegh@gmail.com>
2018-08-29 16:52:56 +0200

    start car simulator module

---
+++ IoT-CarSimulator/IoT-CarSimulator/src/main/java/java/com/simulator/IoTCarSimulatorApplication.java
@@ -0,0 +1,14 @@
+package java.com.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTCarSimulatorApplication 
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(IoTCarSimulatorApplication.class, args);
+	}
+}
